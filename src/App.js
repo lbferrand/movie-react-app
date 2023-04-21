a658f5004f1f1b86e137dec63e7f7f1ea52d6116
lbferrand <lbferrand@gmail.com>
2023-04-21 10:37:07 -0400

    Add dynamic to the app by useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react';
+import MovieCard from './MovieCard';
 
 import './App.css';
 import SearchIcon from './search.svg';
@@ -17,15 +18,18 @@ const movie1 = {
 
 const App = () => {
 
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data.Search);
+    setMovies(data.Search);
   }
 
   useEffect(() => {
-    searchMovies('Spiderman');
+    searchMovies('Superman');
   }, []);
 
   return (
@@ -35,32 +39,29 @@ const App = () => {
       <div className='search'>
         <input
           placeholder='Search for movies'
-          value='Superman'
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
         <img
           src={SearchIcon}
           alt='search'
-          onClick={() => {}}
+          onClick={() => searchMovies(searchTerm)}
         />
       </div>
-      <div className='container'>
-        <div className='movie'>
-          <div>
-            <p>{movie1.Year}</p>
-          </div>
 
-          <div>
-            <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}/>
-          </div>
-
-          <div>
-            <span>{movie1.Type}</span>
-            <h3>{movie1.Title}</h3>
-          </div>
-          
-        </div>
-      </div>
+      { movies?.length > 0
+          ? (
+            <div className='container'>
+              {movies.map((movie) => (
+                <MovieCard movie={movie} />
+              ))}
+              {/* <MovieCard movie1={movies[0]}/> */}
+            </div>
+          ) : (
+            <div className='empty'>
+              <h2>No movies found</h2>
+            </div>
+          )}   
     </div>
   );
 }
