d5bbd08417c9f919f60b41d2b7ea9032d272ef08
lbferrand <lbferrand@gmail.com>
2023-04-20 20:50:59 -0400

    Create App file and add style css file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import './App.css';
+import { useEffect } from 'react'
 
-function App() {
+import './App.css';
+import SearchIcon from './search.svg';
+
+// 4d861d85
+
+const API_URL = 'http://www.omdbapi.com?apikey=4d861d85';
+
+const App = () => {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  }
+
+  useEffect(() => {
+    searchMovies('Under the Tuscan Sun');
+  }, []);
+
   return (
-    <div className="App">
-      
+    <div className='app'>
+      <h1>Movie Land</h1>
+
+      <div className='search'>
+        <input
+          placeholder='Search for movies'
+          value='Superman'
+          onChange={() => {}}
+        />
+        <img
+          src={SearchIcon}
+          
+        />
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
